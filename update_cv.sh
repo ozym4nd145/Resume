d490985bde270661dc04ed6669cbd635f5357dad
ozym4nd145 <ozym4nd145@outlook.com>
2018-02-03 11:21:35 +0530

    Added update script

---
+++ update_cv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pdflatex resume.tex
+scp resume.pdf cs1150262@palasi.cse.iitd.ac.in:~/public_html/Suyash_CV.pdf
+scp resume.pdf suyash@suyash.cse.iitd.ac.in:/var/www/html/Suyash_CV.pdf
