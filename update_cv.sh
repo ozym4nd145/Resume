e25c36710c8e042d2863c050c25f6b882be25d42
Suyash Agrawal <ozym4nd145@outlook.com>
2018-07-29 21:15:19 +0530

    Updated CV for placements

--- update_cv.sh
+++ update_cv.sh
@@ -2,4 +2,4 @@
 
 pdflatex resume.tex
 scp resume.pdf cs1150262@palasi.cse.iitd.ac.in:~/public_html/Suyash_CV.pdf
-scp resume.pdf suyash@suyash.cse.iitd.ac.in:/var/www/html/Suyash_CV.pdf
+#scp resume.pdf suyash@suyash.cse.iitd.ac.in:/var/www/html/Suyash_CV.pdf
